f1306436009ed53c1e830a1769483791079b473a
t nakata <git.nakata0615@gmail.com>
2019-08-17 17:29:36 +0900

    UserService不要なので削除

--- src/main/kotlin/com/example/restapisample/UserController.kt
+++ src/main/kotlin/com/example/restapisample/UserController.kt
@@ -5,7 +5,6 @@ import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -14,25 +13,25 @@ import java.util.*
 import javax.persistence.*
 
 @RestController
-class UserController @Autowired constructor(private val userService: UserService) {
+class UserController @Autowired constructor(private val userRepository: UserRepository) {
 
     @RequestMapping("/user", method = [RequestMethod.GET])
     fun get(): Users {
-        return Users(userService.findAll(), "ok", "get all user.")
+        return Users(userRepository.findAll(), "ok", "get all user.")
     }
 
     @RequestMapping("/user/create", method = [RequestMethod.POST])
     fun create(@RequestBody user: User): UserCreateResponse {
-        val result = userService.save(user)
+        val result = userRepository.save(user)
         return UserCreateResponse(result, "ok", "create user success!!")
     }
 
     @EventListener
     fun seed(event: ContextRefreshedEvent) {
-        val users = userService.findAll();
+        val users = userRepository.findAll()
         if (users.isEmpty()) {
             val user = User(name = "test.tarou", email = "sample@example.com")
-            userService.save(user)
+            userRepository.save(user)
         }
     }
 }
@@ -64,9 +63,3 @@ data class UserCreateResponse (
 
 @Repository
 interface UserRepository : JpaRepository<User, Long>
-
-@Service
-class UserService(private val userRepository: UserRepository) {
-    fun findAll(): List<User> = userRepository.findAll()
-    fun save(user: User) = userRepository.save(user)
-}
